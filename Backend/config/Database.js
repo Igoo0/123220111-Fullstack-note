0b9896c0df77306e1e1697245782dd4c8bc91b5c
Igoo0 <121849968+Igoo0@users.noreply.github.com>
2025-05-19 18:52:35 +0700

    Update Database.js

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize('RECOVER_YOUR_DATA', 'root', '',{
-    host: '34.101.124.245',
+    host: '34.45.173.63',
     dialect: 'mysql'
 });
 
